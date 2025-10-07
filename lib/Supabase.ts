1bf917bc6b89b21d506b46798a52971c738615b7
Amina Otmane Telba <aminaotmanetelba@gmail.com>
2025-10-07 07:43:30 +0100

    Delete lib directory

--- lib/Supabase.ts
+++
@@ -1,7 +0,0 @@
-// lib/Supabase.ts
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://gggopknzksqvzisxylts.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZ29wa256a3Nxdnppc3h5bHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1ODg3MjAsImV4cCI6MjA3NTE2NDcyMH0.JD6Lt61P9tDhjjKHtj3n299GJ1DD6-oJTJMu3BNJ-Jg'
-);
